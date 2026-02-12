1595d617dbab51b7bcc38901eac7f552df106cc8
nelait <47431352+nelait@users.noreply.github.com>
2026-02-11 23:56:44 -0800

    Migration: php-to-react — add src/components/Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="well text">
+      <h2>
+        Training With Live Project
+        <div className="pull-right">
+          Add Me: Facebook.com/S44F4Y4T
+        </div>
+      </h2>
+    </div>
+  );
+};
+
+export default Footer;
